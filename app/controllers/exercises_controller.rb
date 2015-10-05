d4881359f6a9cd48aa6202f319e97403b27b345b
Casey Rieder <casey.rieder@gmail.com>
2015-10-05 18:55:48 +0000

    first attempt at rendering exercises

---
+++ app/controllers/exercises_controller.rb
@@ -0,0 +1,12 @@
+class ExercisesController < ApplicationController
+
+  def index  
+    # care_raw = File.read('#{Rails.root}/app/assets/data/careItems.json')
+    # care_hash = JSON.parse(care_raw)
+    
+    # render :file => "#{Rails.root}/app/assets/data/careItems.json", 
+    #   :content_type => 'application/json',
+    #   :layout => false
+  end
+  
+end
