f678262590582d4760cbec0256ff748b0ca473a9
Casey Rieder <casey.rieder@gmail.com>
2015-10-09 19:29:29 +0000

    Added associatino for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009192333) do
+ActiveRecord::Schema.define(version: 20151009192818) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20151009192333) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
